18f679fbd11ac6e8acb3ed808719780df0edbab6
Jacek Wydro <jacwydro@gmail.com>
2022-05-10 21:52:41 +0200

    calculator

---
+++ src/main/java/com/example/calculatorgrd/CalculatorGrdApplication.java
@@ -0,0 +1,13 @@
+package com.example.calculatorgrd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatorGrdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CalculatorGrdApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/calculatorgrd/CalculatorGrdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.calculatorgrd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorGrdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
